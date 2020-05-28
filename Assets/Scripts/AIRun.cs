c1a3be39fa7b338045e34bf631cd54f80e551d16
NikolayRomanow <system.program.code@gmail.com>
2020-05-28 21:03:02 +0500

    two commit

---
+++ Assets/Scripts/AIRun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AIRun : MonoBehaviour
+{
+    public Transform CurrentPosition;
+    public Transform[] TargetPosition;
+    public float speed = 1.0f;
+    private int count;
+
+    private void Update()
+    {
+        CurrentPosition.position = Vector3.MoveTowards(CurrentPosition.position, TargetPosition[count].position, speed * Time.deltaTime);
+        CurrentPosition.rotation = Quaternion.RotateTowards(CurrentPosition.rotation, TargetPosition[count].rotation, 5 * Time.deltaTime);
+        if (CurrentPosition.position == TargetPosition[count].position)
+            count++;
+    }
+}
---
+++ Assets/Scripts/Victorine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Victorine : MonoBehaviour
+{
+    public QuestionList[] Questions;
+    public Text qText;
+
+    public void OnClickPlay()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class QuestionList
+{
+    public string question;
+    public string[] answers = new string[3];
+}
